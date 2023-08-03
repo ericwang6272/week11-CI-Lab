3d741e30b140095a0fcb270dafff428d167f4291
Eric Wang <yichieh98@gmail.com>
2023-08-03 17:26:43 +0800

    new test code added

--- unittest/unit_test.py
+++ unittest/unit_test.py
@@ -1,5 +1,13 @@
-def test():
-    pass
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
 
 if __name__ == "__main__":
-    test()
+    unittest.main()
